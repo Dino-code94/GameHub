345de0604091b9f3342c67f1b0f3be669d617402
Dino-code94 <dinoklaric29@gmail.com>
2025-11-08 18:55:16 -0800

    Add logout url

--- gamehub/authapp/urls.py
+++ gamehub/authapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='authapp/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    
 ]
